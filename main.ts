271f1c4a0a3525d3cd732a36da906ccd1c3b0720
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-11-24 12:45:32 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -11,23 +11,23 @@ let distanceToObject: number = 0
 basic.showIcon(IconNames.Happy)
 
 // loop forever
-basic.forever(function() {
-distanceToObject = sonar.ping(
+basic.forever(function () {
+  distanceToObject = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-)
+  )
 })
 
 // loop forever
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
+  if (input.buttonIsPressed(Button.A) === true) {
     // move car forwards if distance is greater than 10
     if (distanceToObject < 10) {
       basic.showIcon(IconNames.Yes)
       robotbit.StpCarMove(10, 48)
     }
-    // move car backwards then turn if distance is less than or equal to 10
+    // move car backwards then tur     n if distance is less than or equal to 10
     if (distanceToObject >= 10) {
       robotbit.StpCarMove(-10, 48)
       robotbit.StpCarTurn(90, 48, 125)
