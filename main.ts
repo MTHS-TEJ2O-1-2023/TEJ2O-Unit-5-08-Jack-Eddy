b7b1266372b6c253d76a2d48aa399e576ef977d0
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-11-28 13:27:38 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -4,34 +4,27 @@
  * Created on: Nov 2023
  * This program moves a stepper motor forward, back and turns based on the distance
 */
+
 // variables
 let distanceToObject: number = 0
 
 // setup
+basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// loop forever
-basic.forever(function () {
-  distanceToObject = sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-  )
-})
-
-// loop forever
 while (true) {
-  if (input.buttonIsPressed(Button.A) === true) {
-    // move car forwards if distance is greater than 10
-    if (distanceToObject < 10) {
-      basic.showIcon(IconNames.Yes)
-      robotbit.StpCarMove(10, 48)
+    if (input.buttonIsPressed(Button.A) === true) {
+        distanceToObject = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+        // move forwards
+        if (distanceToObject > 10) {
+            basic.showString(distanceToObject.toString())
+            robotbit.StpCarMove(10, 48)
+        }
+        // move backwards and turn
+        if (distanceToObject <= 10) {
+            basic.showString(distanceToObject.toString())
+            robotbit.StpCarMove(-10, 48)
+            robotbit.StpCarTurn(90, 48, 125)
+        }
     }
-    // move car backwards then tur     n if distance is less than or equal to 10
-    if (distanceToObject >= 10) {
-      robotbit.StpCarMove(-10, 48)
-      robotbit.StpCarTurn(90, 48, 125)
-      basic.showIcon(IconNames.Happy)
-    }
-  }
 }
